6afb5c74e6f11287c8ff3cd27305517d4553e775
charlo18 <boudreaultca@gmail.com>
2026-04-18 01:13:47 -0400

    basic training done , no saving yet

---
+++ content/TrainTest.py
@@ -0,0 +1,41 @@
+from names import names
+import random
+from torch import nn
+import utils
+from torch.utils.data import DataLoader
+import torch
+import tqdm
+
+
+class TrainTestManager:
+    def __init__(self, model_name: str, network: nn.module):
+        if model_name is None:
+            model_name = random.choices(names) + str(random.randint(0, 10000))
+        self.model_name = model_name
+        self.network = network
+
+    def train(
+        self,
+        epochs: int,
+        train_dataset: DataLoader,
+        test_dataset: DataLoader,
+        weight_decay: float,
+        momentum: float,
+        loss_function: nn.Module,
+        optimizer: torch.optim = torch.optim.SGD,
+        lr: float = 0.01,
+    ):
+        optim = utils.build_optimizer(
+            self.network, lr, optimizer, momentum, weight_decay
+        )
+        self.network.train()  # training mode
+        for epoch in range(epochs):
+            print(epoch)
+            for images, labels in tqdm.tqdm(train_dataset):
+                pred = self.network(images)  # forward
+                loss = loss_function(pred, labels)  # loss
+                optim.zero_grad()
+                loss.backward()  # backward propagation
+                optim.step()
+
+    def test(self): ...
